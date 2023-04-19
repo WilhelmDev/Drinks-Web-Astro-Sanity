a351cbf8b0d24b3c71633f69ed7b5792b1bc8102
WilhelmDev <wiljoseflores@gmail.com>
2023-04-18 20:18:09 -0400

    Ready

---
+++ client.js
@@ -0,0 +1,30 @@
+import { createClient } from "@sanity/client";
+
+
+export const client = createClient({
+    projectId:import.meta.env.PUBLIC_PROJECT_ID,
+    dataset: 'production',
+    useCdn: false,
+    apiVersion: '2023-03-30',
+    token:import.meta.env.PUBLIC_TOKEN_PROJECT
+})
+
+export async function getRum() {
+    const drinksRum = await client.fetch('*[_type == "drinks" && drink_type[0] == "rum"]')
+    return drinksRum
+}
+
+export async function getWhiskey() {
+    const drinksWhiskey = await client.fetch('*[_type == "drinks" && drink_type[0] == "whiskey"]')
+    return drinksWhiskey
+}
+
+export async function getProduct(url) {
+    const bottleData = await client.fetch(`*[_type == "drinks" && url_product.current == '${url}'][0]`)
+    return bottleData
+}
+
+export async function getAllBottles() {
+    const bottleData = await client.fetch(`*[_type == "drinks"]`)
+    return bottleData
+}
